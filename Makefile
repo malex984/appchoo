b56d0025556c331589bb3ed23e7d9b986acdef4c
Oleksandr Motsak <malex984@googlemail.com>
2015-05-20 01:04:48 +0200

    Quick and dirty hack for improved useability
    FIX: better input text handling
    ADD: if image is not found: try to draw that string (also if it looks like "[^ ]*")
    ADD: -p Option for prompt strimg to render as a header
    ADD: -f Option for .ttf font file
    
    NOTE: we replace '_' -> ' ' in label string before drawing it
    
    NOTE: now additionally requires libSDL_ttf & libSDL_gfx
    (e.g. `DEBIAN_FRONTEND=noninteractive apt-get -q -y install libsdl-image1.2 libsdl-ttf2.0 libsdl-gfx1.2`)

--- Makefile
+++ Makefile
@@ -1,12 +1,11 @@
-
-CFLAGS = -std=c99 -D_GNU_SOURCE=1 -Wall -W -O3 $(shell sdl-config --cflags)
-LDFLAGS = $(shell sdl-config --libs) -lSDL_image
+CFLAGS = -std=c99 -lc -D_GNU_SOURCE=1 -Wall -W -O3 $(shell sdl-config --cflags)
+LDFLAGS = $(shell sdl-config --libs) -lSDL_image  -lSDL_ttf -lSDL_gfx
 
 appchoo: appchoo.c
 	$(CC) -o $@ $< $(CFLAGS) $(LDFLAGS)
 
-test: appchoo
-	exec `echo -e 'duke.jpg echo duke\nbörek.jpg echo börek\ncat.jpg echo cat\nmirror.png echo mirror\ntachikoma.jpg echo tachikoma' | ./appchoo -t 10 -d 'echo timeout'`
+check: appchoo
+	exec `echo '"duke.jpg" echo duke\n"börek.jpg" echo börek\n"cat.jpg" echo cat\n"mirror.png" echo mirror\n"tachikoma.jpg" echo tachikoma' | ./appchoo -t 10 -p 'Prompt!' -f 'FreeMonoBold.ttf' -d 'echo timeout'`
 
 clean:
 	rm -f appchoo
--- appchoo.c
+++ appchoo.c
@@ -5,9 +5,16 @@ To the extent possible under law, the author(s) have dedicated all copyright and
 You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 */
 
-#include <getopt.h>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
+
+#include <SDL_rotozoom.h>
+
+#include <getopt.h>
+#include <string.h>
+
+static TTF_Font* font;
 
 int fit_image(SDL_Surface *image, int w, int h)
 {
@@ -100,6 +107,11 @@ void handle_events(SDL_Surface *screen, SDL_Rect *rects, char **apps, int num, c
 				}
 				break;
 			case SDL_QUIT:
+		                TTF_CloseFont(font);
+		   
+		                TTF_Quit();
+                                SDL_Quit();
+		   
 				exit(1);
 				break;
 			default:
@@ -134,12 +146,15 @@ SDL_Cursor *empty_cursor()
 static int hide_cursor = 0;
 static int timeout = 0;
 static char *to_cmd = "true";
+static char *p = NULL;
+char *fn = "FreeMonoBold.ttf"; // default font
+
 void init(int argc, char **argv)
 {
 	for (;;) {
-		switch (getopt(argc, argv, "hct:d:")) {
+		switch (getopt(argc, argv, "hct:d:p:f:")) {
 			case 'h':
-				fprintf(stderr, "usage: %s [-h] (show help) [-c] (hide cursor) [-t NUM] (timeout after NUM seconds) [-d 'CMD'] (emit 'CMD' instead of 'true' on timeout)\n", argv[0]);
+				fprintf(stderr, "usage: %s [-h] (show help) [-c] (hide cursor) [-t NUM] (timeout after NUM seconds) [-p 'Prompt'] [-f 'FontName.ttf'] [-d 'CMD'] (emit 'CMD' instead of 'true' on timeout)\n", argv[0]);
 				exit(1);
 				break;
 			case 'c':
@@ -150,6 +165,14 @@ void init(int argc, char **argv)
 				break;
 			case 'd':
 				to_cmd = optarg;
+				break;
+			case 'p':
+				p = optarg;
+//		                fprintf(stderr, "prompt: %s\n", p);
+				break;
+			case 'f':
+				fn = optarg;
+//		                fprintf(stderr, "font file : %s\n", fn);
 				break;
 			case -1:
 				return;
@@ -162,46 +185,120 @@ void init(int argc, char **argv)
 	}
 }
 
+#define LENGTH 2048
+
 int check_corner(char *out, char *in, char *which)
 {
-	if (in == strstr(in, which)) {
-		strncpy(out, in + 4, 2048);
-		out[strnlen(out, 2048) - 1] = 0;
-		return 1;
-	}
-	return 0;
+   if (in != strstr(in, which))
+     return 0;
+     
+   int l = strnlen(which, LENGTH);
+   
+   char * s = in + l;
+   
+   while (*s == ' ') 
+     s++;
+   
+   l = strnlen(s, LENGTH);
+	   
+   if( l == 0 )
+     return 0;
+	   
+   strncpy(out, s, l+1);
+// out[strnlen(out, LENGTH) - 1] = 0;
+   return 1;
 }
 
 int main(int argc, char **argv)
 {
 	init(argc, argv);
+   
 	int max = 32;
 	int num = 0;
-	char imgs[max][2048];
+	char imgs[max][LENGTH];
+   
 	char *apps[max];
-	char corners[4][2048];
+	char corners[4][LENGTH];
+   
 	for (int i = 0; i < 4; i++)
 		*corners[i] = 0;
 
-	while (num < max && fgets(imgs[num], 2048, stdin)) {
-		if ('#' == *imgs[num])
-			continue;
-		if (check_corner(corners[0], imgs[num], "@NW"))
-			continue;
-		if (check_corner(corners[1], imgs[num], "@NE"))
-			continue;
-		if (check_corner(corners[2], imgs[num], "@SW"))
-			continue;
-		if (check_corner(corners[3], imgs[num], "@SE"))
-			continue;
-		imgs[num][strnlen(imgs[num], 2048) - 1] = 0;
-		char *delim = strchr(imgs[num], ' ');
-		apps[num] = delim + 1;
-		*delim = 0;
-		num++;
+	while ( (num < max) && fgets(imgs[num], LENGTH, stdin) )
+        {
+	   int l = strnlen(imgs[num], LENGTH);
+	   
+	   if( l == 0 ) continue;
+	   
+	   //	        if( l > LENGTH ) l = LENGTH;
+	     
+	   imgs[num][l - 1] = 0; // ? instead of EOL?
+	       
+	   char *delim = strchr(imgs[num], '#');
+	   
+	   if ( delim != NULL )
+	     *delim = 0;
+	   
+	   char *start = imgs[num];
+	   
+	   while( *start == ' ' ) 
+	     start++;
+
+	   l = strnlen(start, l);
+	   
+	   if( l == 0 ) 
+	     continue;
+	   
+	   if( *start == '@' )
+	     {
+		
+		if (check_corner(corners[0], start, "@NW "))
+		  continue;
+		
+		if (check_corner(corners[1], start, "@NE "))
+		  continue;
+		
+		if (check_corner(corners[2], start, "@SW "))
+		  continue;
+		
+		if (check_corner(corners[3], start, "@SE "))
+		  continue;
+		
+		fprintf(stderr, "Warn: wrong corner in input line %d: '%s'\n", num+1, imgs[num]);
+		continue;
+	     }
+	   
+	   delim = strchr(start, ' '); // todo: single space!?
+	   
+	   while( (delim != NULL) && (*delim == ' ') ) 
+	     delim++;
+	   
+	   if( delim == NULL )
+	     {
+		fprintf(stderr, "Warn: missing ' ' in the input line %d: '%s'\n", num+1, imgs[num]);
+		continue;
+	     } 
+	   apps[num] = delim;
+	   
+	   *(delim-1) = 0;
+	   
+	   l = strnlen(start, l);
+	   
+	   for( ; delim != start ; delim-- )
+	     if ( *delim == '_' )
+	       *delim = ' ';
+	   
+	   strncpy(imgs[num], start, l+1);
+	   
+	   num++;
 	}
 
 	SDL_Init(SDL_INIT_VIDEO);
+   
+        if(TTF_Init() == -1) 
+        {
+	   fprintf(stderr, "TTF_Init: %s\n", TTF_GetError()) ;
+//	   exit(2);
+	}
 
 	const SDL_VideoInfo *const info = SDL_GetVideoInfo();
 
@@ -218,31 +315,115 @@ int main(int argc, char **argv)
 	if (screen->w != w || screen->h != h)
 		exit(1);
 
-	SDL_WM_SetCaption("Application Chooser", "appchoo");
+	SDL_WM_SetCaption("Application Chooser", NULL ) ; // "appchoo");
 
 	if (hide_cursor)
 		SDL_SetCursor(empty_cursor());
 
         SDL_FillRect(screen , NULL , 0xFFFFFF);
-	
+        font = TTF_OpenFont(fn, 25); // todo ADD an option for font file name!
+   
+        if(!font) 
+        {
+	   fprintf(stderr, "TTF_OpenFont (%s): %s\n", fn, TTF_GetError());
+	   // handle error
+	}
+   
+  
 	int num_x = 1;
 	int num_y = 1;
 
-	while (num > (num_x * num_y)) {
+	while (num > (num_x * num_y)) 
+	{
 		if (num_y < num_x)
 			num_y++;
 		else
 			num_x++;
 	}
+	
+	// max num_x/y : num_x * num_y <= num ?
+	// todo: start with num_x = num_y = [sqrt(num)]... ?
 
 	SDL_Rect rects[max];
-	for (int i = 0; i < num; i++) {
-		SDL_Surface *image = IMG_Load(imgs[i]);
+	
+	for (int i = 0; i < num; i++) 
+	{
+	    if( strnlen( imgs[i], LENGTH ) == 0 ) 
+	      continue;
+	      
+	    SDL_Surface *image = NULL;
+	    
+	    if( *imgs[i] != '"' ) 
+	    {
+		image = IMG_Load(imgs[i]);
 
 		if (!image) {
-			fprintf(stderr, "could not load \"%s\"\n", imgs[i]);
-			exit(1);
+		    fprintf(stderr, "could not load \"%s\"\n", imgs[i]);
+//			exit(1);
 		}
+	    }
+		
+    	    if(font && !image) 
+    	    {
+	
+	       // get the width and height of a string as it would be rendered in a loaded font
+/*	   
+	   int ww,hh;
+	   if(TTF_SizeText(font,imgs[i],&ww,&hh)) 
+	     {
+	      // perhaps print the current TTF_GetError(), the string can't be rendered...
+	     } else 
+	     {
+//	        printf("width=%d height=%d\n",ww,hh);
+	     }
+*/
+	   
+	   char *start = imgs[i];
+	       
+	   while( *start == ' ' ) 
+		 start++;
+	       
+	    if( *start == '"' )
+            {
+	      char *stop = ++start;
+	       
+	      while ( (*stop != 0) && (*stop != '"') )
+		stop++;
+	       
+	      *stop = 0;
+	    }
+	       
+	    
+	      
+	   // SDL_Color foregroundColor = { 0, 0, 0 }; SDL_Color backgroundColor = { 0, 0, 255 } ;
+	   // TTF_RenderText_Shaded(font, "This is my text.", foregroundColor, backgroundColor);
+	      
+	   SDL_Color color={0,0,0,0};
+	   SDL_Surface* textSurface =  TTF_RenderUTF8_Blended (font, start, color);
+	  
+	     if(!textSurface) 
+	     {
+		fprintf(stderr, "TTF_Render?_?: %s\n", TTF_GetError());
+	     }
+	     else
+	     {
+//		SDL_BlitSurface(textSurface,NULL,screen,NULL);
+		
+		// SDL_Surface* textSurface2 =  
+		image = rotozoomSurface(textSurface, 45.0, 1.0, SMOOTHING_ON ) ;
+		
+		// Pass zero for width and height to draw the whole surface
+//		SDL_Rect textLocation = { w >> 1, h >> 1, 0, 0 };
+//		SDL_BlitSurface(textSurface2, NULL, screen, &textLocation);
+        
+		//     //perhaps we can reuse it, but I assume not for simplicity.
+
+//		SDL_FreeSurface(textSurface2);
+		SDL_FreeSurface(textSurface);
+	     }
+     }
+		
+    		
 
 		SDL_Rect dest = screen->clip_rect;
 
@@ -252,16 +433,62 @@ int main(int argc, char **argv)
 		dest.x += (i % num_x) * dest.w;
 		dest.y += ((i / num_x) % num_y) * dest.h;
 
+		if( !image )
+		{
+		    /* Creating the surface. */
+		    image = SDL_CreateRGBSurface(0, dest.w >> 1, dest.h >> 1, 32, 0, 0, 0, 0);
+		
+    		    /* Filling the surface with red color. */
+		    SDL_FillRect(image, NULL, SDL_MapRGB(image->format, 200, 0, 0));
+		}
+
 		if (!fit_image(image, dest.w, dest.h))
-			exit(1);
+		{
+			
+//		    	exit(1); // ???
+		}
+		
 
 		SDL_Rect src = image->clip_rect;
 
 		center_image(&dest, &src);
+		
+		SDL_SetAlpha( image, SDL_SRCALPHA, 128 );
 
 		SDL_BlitSurface(image, &src, screen, &dest);
 		SDL_FreeSurface(image);
 		rects[i] = dest;
+	}
+	
+	if( font && p != NULL )
+	{
+	   SDL_Color color={255,0,0,0};
+	   SDL_Surface* textSurface = TTF_RenderUTF8_Blended (font, p, color);
+	  
+	     if(!textSurface) 
+	     {
+		fprintf(stderr, "TTF_Render?_?: %s\n", TTF_GetError());
+	     } else
+	     {
+//                fprintf(stderr, "drawing prompt (%s)\n", p);
+//		image = rotozoomSurface(textSurface, 0.0, 1.0, SMOOTHING_ON ) ;
+		
+		// Pass zero for width and height to draw the whole surface
+		SDL_Rect dest = screen->clip_rect;
+		SDL_Rect src = textSurface->clip_rect;
+
+		dest.h = 30;
+		dest.y = 0;
+		
+		center_image(&dest, &src);
+
+		SDL_SetAlpha( textSurface, SDL_SRCALPHA, 128 );
+		
+//		SDL_BlitSurface(textSurface, NULL, screen, NULL ) ;// &src... &dest);
+		SDL_BlitSurface(textSurface, &src, screen, &dest);
+		
+		SDL_FreeSurface(textSurface);
+	     }	    
 	}
 
 	SDL_Flip(screen);
